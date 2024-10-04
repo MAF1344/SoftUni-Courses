ab4f51a8e8e1e16f02d46f99dd4a8bc8fc0834b0
Muhammad Al Fatih <114471574+MAF1344@users.noreply.github.com>
2024-10-04 11:04:48 +0700

    Delete Pertemuan Ke-14(27-09-2024) directory
    Membersihkan isi repository lalu menggantinya dengan yang baru

--- Pertemuan Ke-14(27-09-2024)/Exercise/Sesi 2/PeriodicTable.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-periodic_table = set()
-
-for num in range(n):
-    elements = input().split()
-    for element in elements:
-        periodic_table.add(element)
-
-print('\n'.join(sorted(periodic_table)))
--- Pertemuan Ke-14(27-09-2024)/Exercise/Sesi 2/UniqueUsernames.py
+++
@@ -1,6 +0,0 @@
-n = int(input())
-unique_names = set()
-for i in range(n):
-    unique_names.add(input())
-for person in unique_names:
-    print(person)
